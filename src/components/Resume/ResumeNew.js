20c41bf2013da1e5b3607a7a95be06681e0c83c1
Silvergab <alfagabriel11@gmail.com>
2025-04-24 10:11:14 +0200

    actualizar CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV Alfonso Moreno 10.04 ESP.pdf";
+import pdf from "../../Assets/../Assets/CV Gabriel Hernandez ESP.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
